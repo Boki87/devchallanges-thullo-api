ee4843690c3cc0d676447464bd332961ab3abcf8
Bojan Peric <boki@iMac-141.local>
2021-04-23 16:23:05 +0200

    .

---
+++ models/comment.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    cardId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Card",
+      required: [true, "Card Id is required to create a comment"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Comment", CommentSchema);
