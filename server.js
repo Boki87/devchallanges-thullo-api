37dd4f2617849652062c122a06cb6e4e399cf9e2
Bojan Peric <boki@iMac-141.local>
2021-04-23 15:55:04 +0200

    enable cors

--- server.js
+++ server.js
@@ -24,7 +24,17 @@ const files = require("./routes/files");
 const app = express();
 
 //cors
-app.use(cors());
+// app.options("*", cors());
+// app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 //body parser
 app.use(express.json());
 
